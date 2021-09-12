191a6c5773c25ccec7cfe9fb3db74056a0273d62
Benjamin Saint-Cyr <benjamin.saintcyr@gmail.com>
2021-09-12 09:34:25 -0400

    rename promela to spin

--- ben/packages/spin.scm
+++ ben/packages/spin.scm
@@ -1,4 +1,4 @@
-(define-module (ben packages promela)
+(define-module (ben packages spin)
   #:use-module (gnu packages)
   #:use-module (guix git-download)
   #:use-module (guix licenses)
@@ -6,11 +6,12 @@
   #:use-module (guix build-system gnu)
   #:use-module (guix download)
   #:use-module (gnu packages bison)
-  #:use-module (gnu packages gcc))
+  #:use-module (gnu packages gcc)
+  #:use-module (gnu packages base))
 
-(define-public promela
+(define-public spin
   (package
-   (name "promela")
+   (name "spin")
    (version "6.5.2")
    (source
     (origin
@@ -33,11 +34,13 @@
                               (lambda* (#:key inputs outputs #:allow-other-keys)
                                 (let ((out (assoc-ref outputs "out")))
                                   (invoke "make" "INSTALL=install -D" (string-append "DESTDIR=" out) "install")))))))
-   (native-inputs
+   (inputs
     `(("gcc" ,gcc)
-      ("bison" ,bison)))
+      ("glibc" ,glibc)))
+   (native-inputs
+    `(("bison" ,bison)))
    (home-page "http://spinroot.com/spin/whatispin.html")
-   (synopsis "Spin is a widely used open-source software verification tool")
+   (synopsis "Formal software verification tool")
    (description
     "Spin is a widely used open-source software verification tool. The tool can be used for the formal verification of multi-threaded software applications. The tool was developed at Bell Labs in the Unix group of the Computing Sciences Research Center, starting in 1980, and has been available freely since 1991. Spin continues to evolve to keep pace with new developments in the field. In April 2002 the tool was awarded the ACM System Software Award")
    (license bsd-3)))
